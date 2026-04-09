74fa706c5569adedda9af75bdfacbf98beef5ea9
Dawid Weiss <dawid.weiss@carrotsearch.com>
2026-04-09 13:07:45 +0200

    Cleanups.

--- randomizedtesting-jupiter/src/main/java/module-info.java
+++ randomizedtesting-jupiter/src/main/java/module-info.java
@@ -3,7 +3,6 @@ import com.carrotsearch.randomizedtesting.jupiter.RepeatExecutionTestEngine;
 module com.carrotsearch.randomizedtesting {
   requires org.junit.jupiter.api;
   requires org.junit.jupiter.params;
-  requires org.junit.platform.engine;
   requires java.logging;
 
   exports com.carrotsearch.randomizedtesting.jupiter;
@@ -13,10 +12,14 @@ module com.carrotsearch.randomizedtesting {
   opens com.carrotsearch.randomizedtesting.jupiter to
       org.junit.platform.commons;
 
-  uses org.junit.platform.engine.TestEngine;
-
   provides org.junit.jupiter.api.extension.Extension with
       com.carrotsearch.randomizedtesting.jupiter.internals.RandomizedContextExtension;
+
+  // These entries install support for tests.iters (RepeatedExecutionTestEngine).
+  requires org.junit.platform.engine;
+
+  uses org.junit.platform.engine.TestEngine;
+
   provides org.junit.platform.engine.TestEngine with
       RepeatExecutionTestEngine;
 }
